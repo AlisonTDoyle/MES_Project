6af5215f3b78482e7901771e3264a558edd609e5
S00233102 <S00233102@atu.ie>
2026-01-13 10:50:39 +0000

    Can fetch all work orders for a time period

--- mes-api/src/controllers/work-order/work-order.ts
+++ mes-api/src/controllers/work-order/work-order.ts
@@ -10,20 +10,42 @@ dotenv.config();
 const _supabaseUrl: string = process.env.SUPABASE_URL || "";
 const _supabaseKey: string = process.env.SUPABASE_KEY || "";
 const _supabase = createClient(_supabaseUrl, _supabaseKey);
-const _workOrderTable:string = process.env.WORK_ORDER_TABLE || "";
+const _workOrderTable: string = process.env.WORK_ORDER_TABLE || "";
 
 // Create
 
 // Read
-export const readWorkOrders = async (req:Request, res:Response) => {
+export const readWorkOrders = async (req: Request, res: Response) => {
     let today = new Date();
 
-    let {data, error} = await _supabase
+    let { data, error } = await _supabase
         .from(_workOrderTable)
         .select("*")
         .eq("scheduleDate", today.toISOString().split('T')[0])
         .order("scheduleDate", { ascending: true });
-        
+
+    if (error) {
+        res.status(400).json({ message: error.message });
+    }
+
+    res.status(200).json({ data });
+}
+
+export const readWorkOrdersForTimePeriod = async (req: Request, res: Response) => {
+    let body = req.body as {timePeriod:number};
+    let timePeriod = body.timePeriod;
+
+    let today = new Date();
+    let endDay = addDays(today, timePeriod);
+
+
+    let { data, error } = await _supabase
+        .from(_workOrderTable)
+        .select("*")
+        .lte("scheduleDate", endDay.toISOString().split('T')[0])
+        .gte("scheduleDate", today.toISOString().split('T')[0])
+        .order("scheduleDate", { ascending: true });
+
     if (error) {
         res.status(400).json({ message: error.message });
     }
@@ -34,3 +56,9 @@ export const readWorkOrders = async (req:Request, res:Response) => {
 // Update
 
 // Delete
+
+// Misc
+function addDays(date:any, days:any) {
+    const newDate = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+    return newDate;
+}
