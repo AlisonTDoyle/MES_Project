4c45fbe0e34a40a1eb17250adb5e150c577e6ccb
S00233102 <S00233102@atu.ie>
2026-01-15 11:22:37 +0000

    Add OEE graph for operator

---
+++ mes-gui/app/(routes)/(operator)/dashboard/_components/operator-oee/oee-graph.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { PieLayer, ResponsivePie } from "@nivo/pie"
+
+let data = [
+    {
+        "id": "Running",
+        "label": "Running",
+        "value": 32,
+        "color": "hsl(159.74 100% 37%)"
+    },
+    {
+        "id": "Offline",
+        "label": "Offline",
+        "value": 10,
+        "color": "hsl(215.38 18% 47%)"
+    }];
+
+export function OeeGraph() {
+    const CenteredMetric: PieLayer<{ id: string; label: string; value: number; color: string }> = ({ centerX, centerY, dataWithArc }) => {
+        const total = dataWithArc.reduce((s, d) => s + d.value, 0);
+        const running = dataWithArc.find(d => d.data.id === 'Running')?.value ?? 0;
+        const percent = total ? Math.round((running / total) * 100) : 0;
+
+        return (
+            // raw svg styling needed here
+            <g transform={`translate(${centerX}, ${centerY})`}>
+                <text
+                    textAnchor="middle"
+                    dominantBaseline="central"
+                    fill="white"
+                    fontWeight={600}
+                    fontSize={32}
+                >
+                    {percent}%
+                </text>
+            </g>
+        );
+    };
+
+    return (
+        <ResponsivePie /* or Pie for fixed dimensions */
+            data={data}
+            colors={{ datum: 'data.color' }}
+            margin={{ top: 0, right: 80, bottom: 200, left: 80 }}
+            startAngle={-90}
+            endAngle={90}
+            cornerRadius={6}
+            innerRadius={0.72}
+            arcLinkLabelsDiagonalLength={0}
+            enableArcLabels={false}
+            enableArcLinkLabels={false}
+            layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends', CenteredMetric]} />
+    )
+}
--- mes-gui/app/(routes)/(operator)/dashboard/page.tsx
+++ mes-gui/app/(routes)/(operator)/dashboard/page.tsx
@@ -2,10 +2,12 @@ import BreakdownForm from "./_components/record-breakdown/record-breakdown";
 import OperatorStationStatusButton from "./_components/operator-station-status-button";
 import UpcomingWorkOrders from "./_components/upcoming-work-orders/upcoming-work-orders";
 import { MachineEventHistory } from "./_components/machine-event-history/machine-event-history";
+import { OperatorOee } from "./_components/operator-oee/operator-oee";
 
 export default function Dashboard() {
     return (
         <div className="grid grid-cols-[33%_33%_33%] grid-rows-2 h-full min-h-0">
+            {/* row 1 */}
             <div className="tile m-2">
                 <UpcomingWorkOrders />
             </div>
@@ -16,6 +18,11 @@ export default function Dashboard() {
 
             <div className="tile m-2 row-span-2 overflow-hidden">
                 <MachineEventHistory />
+            </div>
+
+            {/* row 2 */}
+            <div className="tile m-2">
+                <OperatorOee></OperatorOee>
             </div>
         </div>
     )
