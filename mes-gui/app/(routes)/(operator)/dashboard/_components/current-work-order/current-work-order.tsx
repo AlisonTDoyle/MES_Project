d51d819e336f22a7edbd1140238b39f6d89264aa
S00233102 <S00233102@atu.ie>
2026-01-21 13:11:25 +0000

    Adding current work order fields

--- mes-gui/app/(routes)/(operator)/dashboard/_components/current-work-order/current-work-order.tsx
+++ mes-gui/app/(routes)/(operator)/dashboard/_components/current-work-order/current-work-order.tsx
@@ -1,9 +1,37 @@
+import { PlayIcon, PauseIcon, CheckIcon } from "@heroicons/react/24/solid"
+
 export function CurrentWorkOrder () {
     return (
         <div className="card shadow-sm">
             <div className="card-body">
-                <span className="card-title">Current Work Order</span>
-                
+                <h3>Current Work Order</h3>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>ID</td>
+                            <td>WO-12345678</td>
+                        </tr>
+                        <tr>
+                            <td>Material</td>
+                            <td>Steel</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button className="btn btn-success"><PlayIcon className="w-4"/><span>Start WO</span></button>
+                <button className="btn btn-warning"><PauseIcon className="w-4"/>Pause WO</button>
+                <button className="btn btn-primary"><CheckIcon className="w-4"/><span>Mark WO Complete</span></button>
             </div>
         </div>
     )
