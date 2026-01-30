cd8ed3f4dccacc1b63e754ddd7942a11f276ea37
S00233102 <S00233102@atu.ie>
2026-01-30 14:05:36 +0000

    Added machine types enum

---
+++ mes-api/src/enums/machineType.ts
@@ -0,0 +1,10 @@
+export enum MachineType {
+    Guillotine = 1,
+    Grader = 2,
+    ConveyorBelt = 3,
+    Extruder = 4,
+    Mixer = 5,
+    PackagingMachine = 6,
+    LabelingMachine = 7,
+    Palletizer = 8
+}
