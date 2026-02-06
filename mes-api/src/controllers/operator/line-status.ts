be5f4389be803d034f3c5347b432135c2a1115d3
S00233102 <S00233102@atu.ie>
2026-02-06 14:34:27 +0000

    Refactor operator line status handling; implement MSSQL integration for create and read operations, and add validation for check-in/out status

--- mes-api/src/controllers/operator/line-status.ts
+++ mes-api/src/controllers/operator/line-status.ts
@@ -1,16 +1,17 @@
 // Imports
 import { Request, Response } from "express";
-import { createClient } from '@supabase/supabase-js'
 import dotenv from "dotenv";
+import sql, { IResult } from "mssql";
 import { UpdatedOperatorInformation } from "../../interfaces/request-models/update-op-line-status-req";
+import { dbClientSetup } from "../../misc/db-client-setup";
+import { OperatorLineCheckInOut, ValidateOperatorLineCheckInOut } from "../../interfaces/object-models/dbo/operator-line-check-in-out";
+import { ConvertTimestampToSqlAcceptableFormat } from "../../misc/convert-timestamp-to-sql-acceptable-format";
 
 dotenv.config();
 
 // Properties
-const _supabaseUrl: string = process.env.SUPABASE_URL || "";
-const _supabaseKey: string = process.env.SUPABASE_KEY || "";
-const _supabase = createClient(_supabaseUrl, _supabaseKey)
-const _statusTable: string = process.env.OPERATOR_LINE_STATUS_TABLE || ""
+const _statusTable: string = process.env.OPERATOR_LINE_STATUS_TABLE || "";
+const _schema: string = process.env.MAIN_SCHEMA || "";
 
 // Create
 export const createNewOperatorLineStatusRecord = async (req: Request, res: Response) => {
@@ -22,18 +23,33 @@ export const createNewOperatorLineStatusRecord = async (req: Request, res: Respo
     console.log("Incoming Operator Line Status Update:", statusInformation);
 
     try {
-        // create new operator line status record
-        let { data, error } = await _supabase
-            .from(_statusTable)
-            .insert([{
-                operatorId: operatorId,
-                lineId: statusInformation.lineId,
-                eventType: statusInformation.newStatus,
-                timestamp: timestamp
-            }])
-            .select();
+        let db: sql.ConnectionPool = await dbClientSetup();
 
-        res.status(200).json({ data });
+        let body = req.body;
+        let newCheckInOutStatus: OperatorLineCheckInOut = {
+            operatorId: Number.parseInt(operatorId),
+            lineId: body.lineId,
+            checkedIn: body.newStatus,
+            timestamp: new Date()
+        }
+        const { error } = ValidateOperatorLineCheckInOut(newCheckInOutStatus);
+
+        if (error) {
+            return res.status(400).json({ error: error.message });
+        }
+
+        let query: string = `
+            INSERT INTO ${_schema}.${_statusTable}
+            (operatorId, lineId, checkedIn, timestamp)
+            VALUES
+            (${newCheckInOutStatus.operatorId}, ${newCheckInOutStatus.lineId}, ${newCheckInOutStatus.checkedIn}, ${ConvertTimestampToSqlAcceptableFormat(newCheckInOutStatus.timestamp)})
+        `;
+
+        let result: IResult<any> = await db.request().query(query);
+
+        return res.status(200).json({
+            message: result
+        });
     } catch (error) {
         if (error instanceof Error) {
             res.status(400).json({ message: error.message });
@@ -49,16 +65,20 @@ export const readOperatorLineStatus = async (req: Request, res: Response) => {
     let operatorId = req.params.operatorId as string;
 
     try {
-        // get opertator status
-        let { data, error } = await _supabase
-            .from(_statusTable)
-            .select("*")
-            .eq("operatorId", operatorId)
-            .order("timestamp", { ascending: false })
-            .limit(1)
-            .maybeSingle();
+        let db: sql.ConnectionPool = await dbClientSetup();
 
-        res.status(200).json({ data });
+        let query: string = `
+            SELECT TOP(1) *
+            FROM ${_schema}.${_statusTable}
+            WHERE operatorId = 1
+            ORDER BY timestamp DESC;
+        `;
+
+        let result: IResult<any> = await db.request().query(query);
+
+        return res.status(200).json(
+            result.recordset[0]
+        );
     } catch (error) {
         if (error instanceof Error) {
             res.status(400).json({ message: error.message });
