5493e5dc42d941b4b9d632143ba0fd10b9c848bb
Alison Doyle <S00233102@atu.ie>
2026-03-24 20:26:30 +0000

    remove unnecessary route

--- mes-api/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { refreshToken, signInUser } from "../controllers/auth/auth";
-
-const router: Router = express.Router();
-
-router.post("/login", signInUser);
-router.post("/refresh", refreshToken);
-
-export default router;
