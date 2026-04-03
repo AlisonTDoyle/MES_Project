d8357611b3bda2d83d4a394d21002a9c0faa6d85
S00233102 <S00233102@atu.ie>
2026-04-03 11:32:21 +0100

    new build

--- mes-api/build/enums/machineEventType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MachineEventType = void 0;
-var MachineEventType;
-(function (MachineEventType) {
-    MachineEventType[MachineEventType["Breakdown"] = 1] = "Breakdown";
-    MachineEventType[MachineEventType["Maintenance"] = 2] = "Maintenance";
-    MachineEventType[MachineEventType["FaultNotice"] = 3] = "FaultNotice";
-})(MachineEventType || (exports.MachineEventType = MachineEventType = {}));
